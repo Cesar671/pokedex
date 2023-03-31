8065d0f330e401d0352f0278a7cfd83ddffea2e8
Cesar <cesar.j.pardo.barrientos@gmail.com>
2023-03-31 00:16:26 -0400

    react router v6 added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Main from './pages/Main';
 import PaginateSection from './pages/PaginateSection';
 import Background from './components/Background';
 import Header from './components/Header';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
     <BrowserRouter> 
       <Background></Background>
       <Header></Header>
-      <Main Section={PaginateSection}/>
+      <Routes path='/'>
+        <Route index element={<Main Section={PaginateSection}/>} exact/>
+      </Routes>
     </BrowserRouter>
   
   );
